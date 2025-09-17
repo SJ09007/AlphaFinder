4e87a3ab1164509484b0759f1eb6d9ff00a1283c
Nikhil Garg <nikhilgarg.drb@gmail.com>
2025-09-18 00:08:18 +0530

    added password strength

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,21 +1,22 @@
 const User = require("../models/usersModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
 const registerUser = async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
-        console.log("1");
+        password = req.body.password;
+        if(!validator.isStrongPassword(password, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1})) {
+            return res.status(400).json("Password is not strong enough");
+        }        
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        console.log("email = ", req.body.email);
-
+        
         const existinguser = await User.findOne({ email: req.body.email });
-        console.log("3");
         if(existinguser) {
             return res.status(400).json("User already exists");
         }    
-        console.log(req.body);
-
+        
         const newUser = new User({
             fullname: req.body.fullname,
             username: req.body.username,
@@ -40,6 +41,7 @@ const login = async (req, res) => {
         if(user.isdeleted) {
             return res.status(400).json("User has been deleted");
         }
+
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if(!validPassword) {
             return res.status(400).json("Wrong Password");
